f1f9629ef9feb6d963a124d182c0e30a8d2a225d
dpw6 <dpw6@sfu.ca>
2022-01-09 20:59:24 -0800

    library with book object

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, pages, genre, author_name, publish_date, cover_img) {
     //constructor
+    this.title = title;
+    this.pages = pages;
+    this.genre = genre;
+    this.author_name = author_name;
+    this.publish_date = publish_date;
+    this.cover_img = cover_img;
 }
 
 function addBookToLibrary(){
     //do stuff here
 }
+
+function displayBooks(){
+    //do stuff here
+}
+
